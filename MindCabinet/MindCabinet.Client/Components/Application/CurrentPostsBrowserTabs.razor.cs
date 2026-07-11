ce82d4f592011d3f40b223f0cee8350a21667efc
hamstar <hamstarhelper@gmail.com>
2026-07-11 09:18:56 -0700

    Upgraded/fixed tabs component (needs more testing)

--- MindCabinet/MindCabinet.Client/Components/Application/CurrentPostsBrowserTabs.razor.cs
+++ MindCabinet/MindCabinet.Client/Components/Application/CurrentPostsBrowserTabs.razor.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using MindCabinet.Client.Components.Application.Renders;
-using MindCabinet.Client.Components.Standard;
+using MindCabinet.Client.Components.Standard.Tabs2;
 using MindCabinet.Client.Services;
 using MindCabinet.Client.Services.DataPresenters;
 using MindCabinet.Client.Services.DbAccess;
@@ -15,14 +15,12 @@ namespace MindCabinet.Client.Components.Application;
 
 
 public partial class CurrentPostsBrowserTabs : ComponentBase {
-    //[Inject]
-    //public IJSRuntime Js { get; set; } = null!;
-
     [Inject]
     public LocalClientSessionManager MySessionMngr { get; set; } = null!;
 
 
-    private Tabs Tabs = null!;
+    private Tabs? Tabs = null;
+
 
     private List<PostsBrowser> _PostsBrowsersByTabIndex = new();
 
@@ -50,27 +48,7 @@ public partial class CurrentPostsBrowserTabs : ComponentBase {
 
 
     public void NewTabAtCurrentIndex( PostsContextObject ctx ) {
-        if( this.Tabs is null ) {
-            this.TabsDataSuppliers.Insert( 0, ctx );
-
-            return;
-        }
-        
-        this.TabsDataSuppliers.Insert( this.Tabs.CurrentTabIndex, ctx );
-
-        //
-
-        PostsBrowser browser;
-        (RenderFragment header, RenderFragment content) = this.GenerateTab_Blazor(
-            ctx: ctx,
-            postsBrowser: out browser
-        );
-
-        this.Tabs.InsertTab( header, content, this.Tabs.CurrentTabIndex );
-
-        //
-
-        this._PostsBrowsersByTabIndex.Insert( this.Tabs.CurrentTabIndex, browser );
+        this.TabsDataSuppliers.Insert( this.Tabs?.CurrentTabIndex ?? 0, ctx );
 
         this.StateHasChanged();
     }
---
+++ MindCabinet/MindCabinet.Client/Components/Standard/Tabs2/Tabs.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections;
+
+namespace MindCabinet.Client.Components.Standard.Tabs2;
+
+
+public partial class Tabs : ComponentBase {
+    [Parameter]
+    public int InitialTabIndex { get; set; } = 0;
+
+    public int CurrentTabIndex { get; private set; }
+
+
+    private List<TabEntry> TabsRegistry = new List<TabEntry>();
+
+    private TabEntry? CurrentTab => this.CurrentTabIndex >= this.TabsRegistry.Count
+        ? null
+        : this.TabsRegistry[ this.CurrentTabIndex ];
+
+
+
+	protected override void OnInitialized() {
+		base.OnInitialized();
+
+        this.CurrentTabIndex = this.InitialTabIndex;
+	}
+
+
+
+    public void ChangeTab( int index ) {
+        if( index < 0 || index >= this.TabsRegistry.Count ) {
+            throw new ArgumentOutOfRangeException( nameof(index), $"Index {index} is out of range for TabsRegistry (count: {this.TabsRegistry.Count})" );
+        }
+        this.CurrentTabIndex = index;
+
+        this.StateHasChanged();
+    }
+}
